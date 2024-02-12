4d784fd4f147a58729204919aa3f658c1324b221
QZE7 <68819710+QZE7@users.noreply.github.com>
2024-02-12 20:20:33 +0000

    inicio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function simbolo() {
+    let a = Math.floor(Math.random() * 0 + 1);
+    console.log("El numero del simbolo es: " + a);
+    return a;
+  }
+
+  switch (simbolo()) {
+    case 1:
+      const container = document.getElementsByClassName(".cont-simbolo");
+
+      const img = document.createElement("img");
+      const img_class = container.classList;
+      img_class.add("simbolo");
+      img.src = "../src/assets/img/picas.webp";
+      img.alt = "Logo Picas";
+
+      container.appendChild(img);
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+      break;
+  }
 };
